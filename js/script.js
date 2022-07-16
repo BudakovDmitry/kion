2ca2ac9b6bf16dabd3a32a24761fbaf3d99541b8
Budakov Dmitry <budakov.it@gmail.com>
2022-07-16 23:27:13 +0300

    Add popup

--- js/script.js
+++ js/script.js
@@ -72,4 +72,97 @@ document.addEventListener("DOMContentLoaded", function () {
         input.parentElement.classList.remove("_error");
         input.classList.remove("_error");
     }
+
+    // Popup
+
+    const popupLinks = document.querySelectorAll(".popup-link");
+    const body = document.querySelector("body");
+    const lockPadding = document.querySelectorAll(".lock-padding");
+
+    let unlock = true;
+
+    const timeout = 800;
+
+    if (popupLinks.length > 0) {
+        for (let i = 0; i < popupLinks.length; i++) {
+            const popupLink = popupLinks[i];
+            popupLink.addEventListener("click", function (e) {
+                const popup = document.getElementById("popup");
+                popupOpen(popup);
+            });
+        }
+    }
+
+    const popupCloseIcon = document.querySelectorAll(".popup__close");
+    if (popupCloseIcon.length > 0) {
+        for (let i = 0; i < popupCloseIcon.length; i++) {
+            const el = popupCloseIcon[i];
+            el.addEventListener("click", function (e) {
+                popupClose(el.closest(".popup"));
+                e.preventDefault();
+            });
+        }
+    }
+
+    function popupOpen(popup) {
+        if (popup && unlock) {
+            const popupActive = document.querySelector(".popup.open");
+            if (popupActive) {
+                popupClose(popupActive, false);
+            } else {
+                bodyLock();
+            }
+            popup.classList.add("open");
+            popup.addEventListener("click", function (e) {
+                if (!e.target.closest(".popup-content")) {
+                    popupClose(e.target.closest("popup"));
+                }
+            });
+        }
+    }
+
+    function popupClose(popupActive, doUnlock = true) {
+        if (unlock) {
+            popupActive.classList.remove("open");
+            if (doUnlock) {
+                bodyUnlock();
+            }
+        }
+    }
+
+    function bodyLock() {
+        const lockPaddingValue =
+            window.innerWidth -
+            document.querySelector(".wrapper").offsetWidth +
+            "px";
+        if (lockPadding.length > 0) {
+            for (let i = 0; i < lockPadding.length; i++) {
+                const el = lockPadding[i];
+                el.style.paddingRight = lockPaddingValue;
+            }
+        }
+        body.style.paddingRight = lockPaddingValue;
+        body.classList.add("lock");
+
+        unlock = false;
+        setTimeout(function () {
+            unlock = true;
+        }, timeout);
+    }
+
+    function bodyUnlock() {
+        setTimeout(function () {
+            for (let i = 0; i < lockPadding.length; i++) {
+                const el = lockPadding[i];
+                el.style.paddingRight = "0px";
+            }
+            body.style.paddingRight = "0px";
+            body.classList.remove("lock");
+        }, timeout);
+
+        unlock = false;
+        setTimeout(function () {
+            unlock = true;
+        }, timeout);
+    }
 });
